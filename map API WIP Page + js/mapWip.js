3aff4f3db511caebd70cb990d36ca2ec793a79ea
Molly Dembo <molly.dembo@gmail.com>
2026-04-04 11:43:55 -0600

    first map API basic outline

--- map API WIP Page + js/mapWip.js
+++ map API WIP Page + js/mapWip.js
@@ -1,1 +1,11 @@
 
+//Google maps DEMO API key: AIzaSyDG-SM3IvZSMGuJ61kZSexN-uXKUXngP_I
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
+
