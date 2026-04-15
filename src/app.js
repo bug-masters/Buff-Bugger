c103d10686675b7b3e02c5d5cf4fe81ceead65db
Ben Davison <beda7834@colorado.edu>
2026-04-15 15:36:12 -0600

    multer configure

--- src/app.js
+++ src/app.js
@@ -22,6 +22,31 @@ app.set('views', path.join(__dirname, 'views'));
 
 const pgp = require('pg-promise')();
 
+// setup image storage
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function(req, fule, cb){
+    cb(null, 'public/images/');
+  },
+  filename: function(req, file, cb){
+    cb(null, file.originalname+"-"+Date.now());
+  }
+});
+const maxSize = 12 * 1024 * 1024; // 12MB
+const upload = multer({
+  storage: storage,
+  limits: {fileSize: maxSize},
+  fileFilter: function(req, file, cb){
+    const filetypes = /jpeg|jpg|png|webp/;
+    const mimetype = filetypess.test(file.mimetype);
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    if(mimetype && extname){
+      return cb(null, true);
+    }
+    cb("Error: File upload only supports the following filetypes - " + filetypes);
+  }
+}).single('bugImage');
+
 // database configuration
 const dbConfig = {
   host: 'db', // the database server
